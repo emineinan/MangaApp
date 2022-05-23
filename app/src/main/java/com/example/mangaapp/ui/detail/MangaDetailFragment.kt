0be01294f6e1080a9ecf0fb9b6f40e6e1e073d4c
emineinan <emineinanx@gmail.com>
2022-05-23 13:43:24 +0300

    Remove navigate function

--- app/src/main/java/com/example/mangaapp/ui/detail/MangaDetailFragment.kt
+++ app/src/main/java/com/example/mangaapp/ui/detail/MangaDetailFragment.kt
@@ -7,12 +7,10 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.example.mangaapp.data.local.entity.FavoritesEntity
 import com.example.mangaapp.data.remote.response.MangaResponsesData
 import com.example.mangaapp.databinding.FragmentMangaDetailBinding
-import com.example.mangaapp.ui.home.HomeFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -51,14 +49,6 @@ class MangaDetailFragment : Fragment() {
                 "${manga.title} added to favorites!",
                 Toast.LENGTH_SHORT
             ).show()
-
-            navigateHome()
         }
-    }
-
-
-    private fun navigateHome() {
-        val action = MangaDetailFragmentDirections.actionMangaDetailFragmentToHomeFragment()
-        Navigation.findNavController(requireView()).navigate(action)
     }
 }
