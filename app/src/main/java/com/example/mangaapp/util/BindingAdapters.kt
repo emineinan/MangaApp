dfe2e0b2861545e9c8432d73e09824a9b7b11549
emineinan <emineinanx@gmail.com>
2022-05-23 11:19:36 +0300

    Add favorite data check

--- app/src/main/java/com/example/mangaapp/util/BindingAdapters.kt
+++ app/src/main/java/com/example/mangaapp/util/BindingAdapters.kt
@@ -1,7 +1,9 @@
 package com.example.mangaapp.util
 
+import android.annotation.SuppressLint
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -16,7 +18,7 @@ import com.example.mangaapp.ui.adapter.MangaAdapter
 fun bindImage(imageView: ImageView, imageUrl: String?) {
     imageUrl?.let {
         val imageUri = imageUrl.toUri().buildUpon().scheme("https").build()
-        imageView.load(imageUri){
+        imageView.load(imageUri) {
             placeholder(R.drawable.ic_loading)
             error(R.drawable.ic_broken_image)
         }
@@ -24,20 +26,24 @@ fun bindImage(imageView: ImageView, imageUrl: String?) {
 }
 
 @BindingAdapter("listHomeData")
-fun bindHomeRecyclerView(recyclerView: RecyclerView,
-                     data: List<MangaResponsesData>?) {
+fun bindHomeRecyclerView(
+    recyclerView: RecyclerView,
+    data: List<MangaResponsesData>?
+) {
     val adapter = recyclerView.adapter as MangaAdapter?
     adapter?.submitList(data)
 }
 
 @BindingAdapter("listFavoriteData")
-fun bindFavoriteRecyclerView(recyclerView: RecyclerView,
-                     data: List<FavoritesEntity>?) {
+fun bindFavoriteRecyclerView(
+    recyclerView: RecyclerView,
+    data: List<FavoritesEntity>?
+) {
     val adapter = recyclerView.adapter as FavoritesAdapter?
     adapter?.submitList(data)
 }
 
-@BindingAdapter("marsApiStatus")
+@BindingAdapter("mangaApiStatus")
 fun bindStatus(statusImageView: ImageView, status: Status?) {
     when (status) {
         Status.LOADING -> {
@@ -51,6 +57,19 @@ fun bindStatus(statusImageView: ImageView, status: Status?) {
         Status.DONE -> {
             statusImageView.visibility = View.GONE
         }
-        else -> {}
+        else -> {
+        }
+    }
+}
+
+@SuppressLint("SetTextI18n")
+@BindingAdapter("dataCheck")
+fun dataCheck(textView: TextView, data: List<FavoritesEntity>?) {
+    if (data != null) {
+        if (data.isEmpty()) {
+            textView.text = "No favorite manga added."
+        } else {
+            textView.text = ""
+        }
     }
 }
