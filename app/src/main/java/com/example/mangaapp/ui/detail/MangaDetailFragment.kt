f52d6164fd6ef212dfc113d1427b6357c2abfc6c
Emine İNAN <56589369+emineinan@users.noreply.github.com>
2022-06-01 15:10:44 +0300

    Remove toast messages

--- app/src/main/java/com/example/mangaapp/ui/detail/MangaDetailFragment.kt
+++ app/src/main/java/com/example/mangaapp/ui/detail/MangaDetailFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
@@ -58,25 +57,13 @@ class MangaDetailFragment : Fragment() {
         binding.buttonDetailFav.setOnCheckedChangeListener { _, _ ->
             if (!isSearchedFavorite) {
                 manga.isFavorite = true
-                isSearchedFavorite=true
                 binding.buttonDetailFav.setBackgroundResource(R.drawable.ic_favorite)
                 viewModel.setFavorite(manga)
-                Toast.makeText(
-                    requireContext(),
-                    "${manga.title} added to favorites!",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            else if(isSearchedFavorite){
-                isSearchedFavorite=false
+            } else if (isSearchedFavorite) {
                 binding.buttonDetailFav.setBackgroundResource(R.drawable.ic_favorite_non)
                 viewModel.deleteFavorite(manga)
-                Toast.makeText(
-                    requireContext(),
-                    "${manga.title} removed from favorites!",
-                    Toast.LENGTH_SHORT
-                ).show()
             }
+            isSearchedFavorite = !isSearchedFavorite
         }
     }
 }
